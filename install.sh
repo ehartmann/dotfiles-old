6f7159f84c4143eda3b61ecd0a9edb6ded41f974
Eric Hartmann <hartmann.eric@gmail.com>
2014-04-05 16:52:50 +0200

    Add installs

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+git clone https://github.com/gcuisinier/jenv.git ~/.jenv
+curl -L http://install.ohmyz.sh | sh
